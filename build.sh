bd7af8dad28b050f2ae19b26142a041af8bac394
HELLINFIX <mridulsharma57570@gmail.com>
2023-05-09 08:18:09 +0000

    Update built script with an option for dirty build

--- build.sh
+++ build.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-
+clear
 function compile() 
 {
-
+echo COSMOS Kernel, CODENAMED - SPACED
+echo
+sleep 3 >/dev/null
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
+ccache -M 100G >/dev/null
 export ARCH=arm64
 export KBUILD_BUILD_HOST=COSMOS
 export KBUILD_BUILD_USER="INFIX"
 git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang
 
-[ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
+read -p "Wanna do dirty build? (Y/N): " build_type
+if [[ $build_type == "N" || $build_type == "n" ]]; then
+echo Deleting out directory and doing clean Build
+sleep 3 >/dev/null
+rm -rf out && mkdir -p out
+fi
+if [[ $build_type == "Y" || $build_type == "y" ]]; then
+echo Warning :- Doing dirty build
+sleep 3 >/dev/null
+fi
+if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
+if ! [[ $build_type == "N" || $build_type == "n" ]]; then
+echo Invalid Input , Read carefully before typing
+echo Trying to restart script
+sleep 3 >/dev/null
+. build.sh && exit
+fi
+fi
 
 make O=out ARCH=arm64 spaced_defconfig
 
